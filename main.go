bd08281d81829e871979cb12b982d154e100f3f2
Taka <takahito.uchino.1218@gmail.com>
2024-04-09 18:56:44 +0900

    データベース疎通テスト

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"net/http"
 
+	"github.com/Takahito-Uchino/golang-rest-echo/model"
 	"github.com/labstack/echo/v4"
 )
 
+func connect(c echo.Context) error {
+	db, _ := model.DB.DB()
+	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "database connection failed")
+	} else {
+		return c.String(http.StatusOK, "database connect")
+	}
+}
+
 func main() {
 	e := echo.New()
-
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-
+	e.GET("/", connect)
 	e.Logger.Fatal(e.Start(":8080"))
 }
